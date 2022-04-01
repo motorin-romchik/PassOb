5ab7eb7ee8b9db102f1a5b9c19f7447eb4d58303
mr_str <80831520+motorin-romchik@users.noreply.github.com>
2022-04-02 00:19:26 +0700

    Initial commit

---
+++ src/PassOb.java
@@ -0,0 +1,26 @@
+class Test {
+    int a,b;
+
+    Test(int i,int j){
+        this.a = i;
+        this.b = j;
+    }
+
+    boolean equals(Test o){
+        if(o.a == a && o.b == b)
+            return true;
+        else
+            return false;
+    }
+}
+
+public class PassOb {
+    public static void main (String args[]){
+        Test ob1 = new Test(100,22);
+        Test ob2 = new Test(100,22);
+        Test ob3 = new Test(-1,-1);
+
+        System.out.println("b1 == ob2: "+ ob1.equals(ob2));
+        System.out.println("b1 == b3: "+ ob1.equals(ob3));
+    }
+}
